ad04f8201af2fb1360f0a70772f9559c8309ed57
yp <473075889@qq.com>
2019-04-09 10:37:30 +0800

    spring-session 实现session共享

--- redis/src/main/java/com/example/demo/cofig/RedisConfig.java
+++ redis/src/main/java/com/example/demo/cofig/RedisConfig.java
@@ -9,15 +9,14 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * Created by EDZ on 2018/10/17.
  */
 @Configuration
-@EnableRedisHttpSession    //实现session共享需要开启的注解
+//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 30)    //实现session共享需要开启的注解   测试不开启也可以实现session共享
 public class RedisConfig {
-
 
 
     /**
